458e350115342cf26fac9fb79dc3a26f8de395d2
Zénó Nagy <nagy.zeno999@gmail.com>
2020-12-07 15:51:57 +0100

    Roundbutton

---
+++ IRF_beadando/IRF_beadando/RoundButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace IRF_beadando
+{
+    public class RoundButton: Button
+    {
+        protected override void OnPaint(System.Windows.Forms.PaintEventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddEllipse(0, 0, ClientSize.Width, ClientSize.Height);
+            this.Region = new System.Drawing.Region(path);
+            base.OnPaint(e);
+        }
+    }
+}
