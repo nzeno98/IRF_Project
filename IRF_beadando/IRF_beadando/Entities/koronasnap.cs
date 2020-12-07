3319dd3c933da637fdadbb15461f67f8600a83d0
Zénó Nagy <nagy.zeno999@gmail.com>
2020-12-07 11:16:53 +0100

    Fájl+beolvasás+readme

---
+++ IRF_beadando/IRF_beadando/Entities/koronasnap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_beadando.Entities
+{
+    public class koronasnap
+    {
+        public DateTime nap { get; set; }
+        public int napibeteg { get; set; }
+        public int napihalott { get; set; }
+        public int osszes { get; set; }
+    }
+}
--- IRF_beadando/IRF_beadando/Form1.cs
+++ IRF_beadando/IRF_beadando/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using IRF_beadando.Entities;
+using System.IO;
 
 namespace IRF_beadando
 {
     public partial class Form1 : Form
     {
+        List<koronasnap> koronasnapok = new List<koronasnap>();
         public Form1()
         {
             InitializeComponent();
+            beolvasas();
+        }
+
+        void beolvasas()
+        {
+            using (StreamReader sr = new StreamReader("korona.csv"))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] adatok = sr.ReadLine().Split(';');
+                    koronasnap k = new koronasnap();
+                    k.nap = Convert.ToDateTime(adatok[0]);
+                    k.napibeteg = Convert.ToInt32(adatok[1]);
+                    k.napihalott = Convert.ToInt32(adatok[2]);
+                    k.osszes = Convert.ToInt32(adatok[3]);
+                    koronasnapok.Add(k);
+
+                }
+
+            };
+
         }
     }
 }
